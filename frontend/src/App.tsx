047706d4af685f72af1761cee0e7947d8cd89c6e
bapplejax <jake.bapple@gmail.com>
2025-07-02 09:20:32 -0500

    CA-90_unit-tests: Updating header import.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import {Header} from "./components/header/header";
+import { Header } from "./components/header/header";
 import { Footer } from "./components/footer/footer";
 import { Home } from "./pages/home/home";
 import { About } from "./pages/About";
